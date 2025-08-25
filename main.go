66d44fd2b095b0bb88f73e3db3641a47fb313904
Otido Peter <peterotido@gmail.com>
2025-08-25 13:24:44 +0300

    making code re-usable

--- main.go
+++ main.go
@@ -9,26 +9,26 @@ import (
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() (float64, error) {
-	data, err := os.ReadFile(accountBalanceFile)
+func getFloatFromFile(fileName string) (float64, error) {
+	data, err := os.ReadFile(fileName)
 	if err != nil {
 		return 1000, errors.New("Failed to find file")
 	}
-	balanceText := string(data)
-	balance, err := strconv.ParseFloat(balanceText, 64)
+	valueText := string(data)
+	value, err := strconv.ParseFloat(valueText, 64)
 	if err != nil {
 		return 1000, errors.New("Failed to parse value to float")
 	}
-	return balance, nil
+	return value, nil
 }
 
-func writeBalanceToFile(balance float64) {
-	balanceText := fmt.Sprint(balance)
-	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+func writeFloatToFile(fileName string, value float64) {
+	valueText := fmt.Sprint(value)
+	os.WriteFile(fileName, []byte(valueText), 0644)
 }
 
 func main() {
-	accountBalance, err := getBalanceFromFile()
+	accountBalance, err := getFloatFromFile(accountBalanceFile)
 	if err != nil {
 		fmt.Println("ERROR")
 		fmt.Println(err)
@@ -58,7 +58,7 @@ func main() {
 
 			*accountBalancePtr += depositAmount
 			fmt.Println("Your updated account balance:", *accountBalancePtr)
-			writeBalanceToFile(*accountBalancePtr)
+			writeFloatToFile(accountBalanceFile, *accountBalancePtr)
 		case 2:
 			var withdrawalAmount float64
 			fmt.Print("Your Withdrawal Amount: ")
@@ -77,7 +77,7 @@ func main() {
 
 			*accountBalancePtr -= withdrawalAmount
 			fmt.Println("Your updated account balance:", *accountBalancePtr)
-			writeBalanceToFile(*accountBalancePtr)
+			writeFloatToFile(accountBalanceFile, *accountBalancePtr)
 		case 3:
 			fmt.Println("Your Account Balance:", *accountBalancePtr)
 		default:
